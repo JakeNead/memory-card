65016e853259373e621dc738157ec8d902dccbee
Jake Nead <jakenead@gmail.com>
2024-03-10 15:36:18 -0700

    Init commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>Memory Game</h1>
+      <span>
+        Get points by clicking on images, but don&apos;t click the same one
+        twice! See if you can get all 12!
+      </span>
+    </header>
+  );
+}
