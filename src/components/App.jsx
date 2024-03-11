b9bccfce4df35335850e986611ca116d5ec9cdfe
Jake Nead <jakenead@gmail.com>
2024-03-10 17:35:26 -0700

    Lifted state from Main to App so Header has access to scores

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,75 @@
 import { useState } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import mockPicArray from "./mockData";
+
 export default function App() {
+  const [picArray, setPicArray] = useState(Array.from({ length: 12 }));
+  const [availableCards, setAvailableCards] = useState([]);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
+  const getData = () => mockPicArray();
+  //   fetch(
+  //     `https://api.unsplash.com/photos/random/?client_id=ilZSWZPGk4_RCtBf634WmDEcGio2Iy2No4CHPnt28EE
+  // `,
+  //     { mode: "cors" }
+  //   ).then((resolve) => {
+  //     return resolve.json();
+  //   });
+
+  const updatePicArray = () => {
+    const newPicArray = [];
+    return Promise.all(
+      picArray.map((a, i) => {
+        return getData()[i];
+      })
+    ).then((resolve) => {
+      resolve.map((item) => {
+        newPicArray.push(item);
+      });
+      setPicArray(newPicArray);
+      setAvailableCards(newPicArray.map((i) => i.id));
+    });
+  };
+
+  function handleClick(e) {
+    setPicArray(shuffleArray(picArray));
+    updateScore(e);
+    updateBest();
+  }
+
+  function shuffleArray(arr) {
+    const a = [...arr];
+    for (let i = 0; i < a.length; i++) {
+      let j = Math.floor(Math.random() * i);
+      [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+  }
+
+  function updateScore(e) {
+    const key = e.target.key;
+    if (availableCards.includes(key)) {
+      setCurrentScore(currentScore + 1);
+    }
+  }
+
+  function updateBest() {
+    if (currentScore - bestScore >= 0) {
+      setBestScore(bestScore + 1);
+    }
+  }
+
   return (
     <>
-      <Header />
-      <Main />
+      <Header currentScore={currentScore} bestScore={bestScore} />
+      <Main
+        getData={getData}
+        picArray={picArray}
+        updatePicArray={updatePicArray}
+        handleClick={handleClick}
+      />
     </>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,49 +2,10 @@ import { useState, useEffect } from "react";
 import Cards from "./Cards";
 import mockPicArray from "./mockData";
 
-export default function Main() {
-  const [picArray, setPicArray] = useState(Array.from({ length: 12 }));
-  // const [availableCards, setAvailableCards]= useState([])
-
+export default function Main({ picArray, updatePicArray, handleClick }) {
   useEffect(() => {
-    const newPicArray = [];
-
-    const getData = () => mockPicArray();
-    //   fetch(
-    //     `https://api.unsplash.com/photos/random/?client_id=ilZSWZPGk4_RCtBf634WmDEcGio2Iy2No4CHPnt28EE
-    // `,
-    //     { mode: "cors" }
-    //   ).then((resolve) => {
-    //     return resolve.json();
-    //   });
-
-    const updatePicArray = () => {
-      return Promise.all(
-        picArray.map((a, i) => {
-          return getData()[i];
-        })
-      ).then((resolve) => {
-        resolve.map((item) => {
-          newPicArray.push(item);
-        });
-        setPicArray(newPicArray);
-      });
-    };
     updatePicArray();
   }, []);
-
-  function handleClick() {
-    setPicArray(shuffleArray(picArray));
-  }
-
-  const shuffleArray = (arr) => {
-    const a = [...arr];
-    for (let i = 0; i < a.length; i++) {
-      let j = Math.floor(Math.random() * i);
-      [a[i], a[j]] = [a[j], a[i]];
-    }
-    return a;
-  };
 
   return (
     <main>
