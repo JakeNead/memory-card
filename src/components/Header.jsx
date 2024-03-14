b4e2dc962f762af1e88e60c8873b8a0e8bf97b82
Jake Nead <jakenead@gmail.com>
2024-03-13 21:29:52 -0700

    Images are now responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,17 @@
 export default function Header({ currentScore, bestScore }) {
   return (
     <header>
-      <div className="left">
-        <h1>Memory Game</h1>
-        <span>
-          Get points by clicking on images, but don&apos;t click the same one
-          twice! See if you can get all 12!
-        </span>
-      </div>
-      <div className="right">
-        <p className="currentScore">Current Score: {currentScore}</p>
-        <p className="bestScore">Best Score: {bestScore}</p>
+      <div className="headerContainer">
+        <div className="left">
+          <h1>Memory Game</h1>
+          <span>
+            Don&apos;t click the same photo twice... See if you can get all 12!
+          </span>
+        </div>
+        <div className="right">
+          <p className="currentScore">Current Score: {currentScore}</p>
+          <p className="bestScore">Best Score: {bestScore}</p>
+        </div>
       </div>
     </header>
   );
